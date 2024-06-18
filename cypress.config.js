85487cfff26c90731fd633a6f53be3de2b6f8ab9
chihabov <94179239+chihabov@users.noreply.github.com>
2024-06-18 12:30:15 +0200

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,10 @@
-
-
-
-
 module.exports = {
     // Autres configurations...
-    integrationFolder: 'cypress/e2e',
     testFiles: '**/*.cy.{js,jsx,ts,tsx}',
+    // Configuration pour les tests end-to-end (e2e)
+    e2e: {
+        testFiles: '**/*.cy.{js,jsx,ts,tsx}',
+        // D'autres configurations spécifiques aux tests e2e
+    },
     // Autres configurations...
 }

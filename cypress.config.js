d10d9bca8d1252a47e21309cc2ddbd30e4dace84
chihabov <chihabovlek@gmail.com>
2024-06-18 10:56:48 +0200

    deuxieme modif

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,27 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
+const mochawesome = require('cypress-mochawesome-reporter/plugin');
+
+module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir: 'cypress/results',
+    overwrite: false,
+    html: false,
+    json: true
+  },
+  e2e: {
+    setupNodeEvents(on, config) {
+      mochawesome(on);
+      // Vous pouvez ajouter d'autres événements ici si nécessaire
+    },
+    baseUrl: 'http://localhost:8080',
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}'
+  }
+});
+
+
+
+/*const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
@@ -6,4 +29,4 @@ module.exports = defineConfig({
       // implement node event listeners here
     },
   },
-});
+});*/
